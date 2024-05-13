55d084fefb720ba1fd76c2cfecfddc54f692c754
nile1801 <ducati1801@gmail.com>
2024-05-13 14:12:48 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,6 +41,10 @@ git clone https://github.com/Ljzkirito/adguardhome-openwrt feeds/packages/net/ad
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='T360'/g" package/base-files/files/bin/config_generate
 sed -i "s/timezone='UTC'/timezone='Asia\/Ho_Chi_Minh'/g" package/base-files/files/bin/config_generate
-sed -i "s/add_list system.ntp.server='time1.apple.com'/add_list system.ntp.server='0.asia.pool.ntp.org'/g" package/base-files/files/bin/config_generate
-sed -i "s/add_list system.ntp.server='time1.google.com'/add_list system.ntp.server='1.asia.pool.ntp.org'/g" package/base-files/files/bin/config_generate
+sed -i 's/time1.apple.com/0.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/1.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/2.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/3.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+
+
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By NILE $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
