f08b7e615ae15666185d51eab7addc949ccd74cd
nile1801 <ducati1801@gmail.com>
2024-05-14 21:54:24 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i "s/UTC/Asia/Ho_Chi_Minh/" package/base-files/files/bin/config_generate
+sed -i "s/UTC/Asia\/Ho Chi Minh/g" package/base-files/files/bin/config_generate
 sed -i 's/time1.apple.com/0.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
 sed -i 's/time1.google.com/1.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
 sed -i 's/time.cloudflare.com/2.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
