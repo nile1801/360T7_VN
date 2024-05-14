76fea6801499dc3d4bd062d31f0ee5e199de4983
nile1801 <ducati1801@gmail.com>
2024-05-14 14:13:39 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
+#sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
 
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,hysteria}
+#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,hysteria}
+
+echo 'src-git kanzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git goabroad https://github.com/kenzok8/small' >>feeds.conf.default
